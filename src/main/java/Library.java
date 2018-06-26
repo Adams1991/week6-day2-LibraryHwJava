2e3755ad2942558f3d9e469a3a77df878acc7975
Adams1991 <adams121@hotmail.co.uk>
2018-06-26 15:25:19 +0100

    book count able to check if empty

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> shelf;
+
+    public Library() {
+        this.shelf = new ArrayList<>();
+    }
+
+    public int bookCount() {
+        return this.shelf.size();
+    }
 }
